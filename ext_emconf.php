3a41ee64516bfb05eeb6d48b3455e19a45f792ea
Trenker <contact@grossberger-ge.org>
2013-12-24 15:49:38 +0100

    Release version 0.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,11 +1,21 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = array(
+/***************************************************************
+ * Extension Manager/Repository config file for ext "better_tables".
+ *
+ * Auto generated 28-09-2013 17:05
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'Better tables',
 	'description' => 'Overrides the default table element with a better, easier to use and modern plugin. Features a JS based wizard, a fluid template and more useable settings.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'dependencies' => '',
+	'dependencies' => 'extbase,fluid,css_styled_content',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
@@ -22,20 +32,26 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '0.0.1',
-	'constraints' => array(
-		'depends' => array(
+	'version' => '0.1.0',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
 			'php' => '5.3.2-5.5.99',
-			'extbase' => '6.1.0-6.2.99',
-			'fluid' => '6.1.0-6.2.99',
-			'css_styled_content' => '6.1.0-6.2.99',
+			'typo3' => '6.1.0-6.1.99',
+			'extbase' => '6.1.0-6.1.99',
+			'fluid' => '6.1.0-6.1.99',
+			'css_styled_content' => '6.1.0-6.1.99',
 		),
-		'conflicts' => array(
+		'conflicts' => 
+		array (
 		),
-		'suggests' => array(
+		'suggests' => 
+		array (
 		),
 	),
-	'suggests' => array(
+	'suggests' => 
+	array (
 	),
 );
 
