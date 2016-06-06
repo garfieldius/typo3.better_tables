ba5e58032e273ce267d08cffb2580214bbe642c5
pixelbrackets <mail@pixelbrackets.de>
2016-06-06 14:02:23 +0200

    [FEATURE] Remove outdated fields from extension config
    Clean up the ext_emfconf file and remove deprecated
    fields and reorder constraints.

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,37 +4,23 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Better tables',
 	'description' => 'Overrides the default table element with a better, easier to use and modern plugin. Features a JS based wizard, a fluid template and more useable settings.',
 	'category' => 'plugin',
-	'shy' => 0,
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
+	'version' => '0.1.0',
 	'state' => 'beta',
-	'internal' => 0,
 	'uploadfolder' => 0,
 	'createDirs' => '',
-	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
-	'lockType' => '',
 	'author' => 'Georg Großberger',
 	'author_email' => 'contact@grossberger-ge.org',
 	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'version' => '0.1.0',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.3.8-5.5.99',
-			'extbase' => '6.1.0-6.2.99',
-			'fluid' => '6.1.0-6.2.99',
+			'typo3' => '6.1.0-6.2.99',
+			'php' => '5.3.0-5.5.99'
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
-		),
-	),
-	'suggests' => array(
+		)
 	),
 );
 
